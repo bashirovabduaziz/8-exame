b9c8cd45b6f09247fb34026a01f6725d598cfed9
bashirovabduaziz <bashirovabduaziz66@gmail.com>
2024-06-23 00:19:12 +0500

    first day

---
+++ src/pages/Cureer/Cureer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cureer = () => {
+  return (
+    <div>Cureer</div>
+  )
+}
+
+export default Cureer
---
+++ src/pages/Guarantees/Guarantees.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Guarantees = () => {
+  return (
+    <div>Guarantees</div>
+  )
+}
+
+export default Guarantees
