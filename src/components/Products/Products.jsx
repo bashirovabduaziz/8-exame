433396a30b64e999a6522580dfbc718be6a53fef
bashirovabduaziz <bashirovabduaziz66@gmail.com>
2024-06-26 21:08:25 +0500

    second day tugab qoldi

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,10 +3,15 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { productData } from '../../static/data';
-import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
+import { FaHeart, FaLongArrowAltLeft, FaLongArrowAltRight, FaRegHeart } from "react-icons/fa";
+import { IoBarChartOutline, IoHeartOutline , IoHeart} from "react-icons/io5";
+import { toggleToWishes } from "../../context/wishlistSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const ProductsData = ({ category }) => {
+  const wishes = useSelector(state => state.wishlist.value);
   let sliderRef = useRef(null);
+  const dispatch = useDispatch();
 
   const next = () => {
     sliderRef.slickNext();
@@ -77,17 +82,31 @@ const ProductsData = ({ category }) => {
         >
           {filteredProducts.map((el) => (
             <div className="px-1 md:px-2 mt-[5px] " key={el.id}>
-              <div className={`flex flex-col items-center rounded-[10px] border-[1px] md:h-[400px] h-[230px] gap-[20px]`}>
+              <div className={`flex flex-col items-center rounded-[10px] border-[1px] md:h-[440px] h-[250px] gap-[20px]`}>
                 <div className="md:h-[280px] h-[250px] w-full items-center rounded-t-[10px] bg-white pt-[20px] flex flex-col">
-                  <div className={`flex items-center md:ml-[-120px] justify-center`}>
+                  <div className={`flex  items-center md:w-[250px] justify-between w-[130px]`}>
                     <p className={` ${getCategoryClass(el.category)} text-[10px] md:text-[17px]` }>{el.category}</p>
+                  <div className="gap-[6px] flex">
+                  <button className=""><IoBarChartOutline className="md:w-[25px] md:h-[25px]" /></button>
+                  <button onClick={() => dispatch(toggleToWishes(el))}> {
+                        wishes.some(w => w.id === el.id) ?
+                            <IoHeart className='md:w-[25px] md:h-[25px] text-green-500' /> :
+                            <IoHeartOutline className='md:w-[25px] md:h-[25px]' />
+                    }</button>
+                  </div>
                   </div>
                   <img src={el.img} alt={el.title} className="mx-auto block md:w-[300px] md:h-[220px] h-[80px] mt-[10px] " />
                 </div>
                 <div className="rounded-b-[10px] bg-[#F8F7F3] px-[15px] py-[14px] sm:py-[15px] md:px-[20px] md:py-[20px]">
-                  <p className="text-[12px] font-medium text-[#202020] mt-[-30px] md:text-[16px] md:w-[200px] md:h-[50px] w-[100px] overflow-hidden text-ellipsis whitespace-nowrap">
+                  <p className="text-[12px] font-medium text-[#202020] mt-[-30px] md:text-[16px] md:w-[200px] md:h-[30px] w-[100px] overflow-hidden text-ellipsis whitespace-nowrap">
                     {el.title}
                   </p>
+                <div className="flex items-end py-[10px]">
+                <p className="text-[14px] md:text-[18px]">{el.price}руб.</p>
+                <del className="text-red-500 md:text-[15px] text-[12px] ml-[5px]">{el.oldprice}</del>
+                </div>
+                <button className=" rounded-[50px] border-[2px] border-[#D5D1E1] md:px-5 px-2 text-[10px] md:text-[18px] py-[5px] text-[#202020] 
+                 hover:border-[#07745E] duration-150 focus:bg-[#E1EFE6] focus:text-[#07745E]">Добавить в корзину</button>
                 </div>
               </div>
             </div>
--- src/static/data.js
+++ src/static/data.js
@@ -10,190 +10,231 @@ export const productData = [
         id:1,
         title: 'Анализатор мочи MIND UA-66',
         img: `${Photo}`,
-        category: 'Новинки'        
+        category: 'Новинки'    ,
+        price: 300000    
     },
     {
         id:2,
         title: 'Ветеринарное оборудование',
         img: `https://global-mt.ru/upload/iblock/85e/o3f2bd3fcsty3x0ykxn00ki55gjdkb3r/metrologya7.jpg`,
-        category: 'Акции'        
+        category: 'Акции'      ,
+        price: 24000,
+        oldprice: 28000  
     },
     {
         id:3,
         title: 'Эндоскопия',
         img: `${Photo2}`,
-        category: 'Новинки'      
+        category: 'Новинки'    ,
+        price: 392300  
     },
     {
         id:4,
         title: 'Оборудование для анастезиологии',
         img: `${Photo4}`,
-        category: 'Новинки'          
+        category: 'Новинки'    ,
+        price: 56000      
     },
     {
         id:5,
         title: 'Для медицинской метрологии',
         img: `${Photo5}`,
-        category: 'Акции'                
+        category: 'Акции'      ,
+        price: 220000 ,
+        oldprice:224000         
     },
     {
         id:6,
         title: 'Мебель для медецинских целей',
         img: `${Photo6}` ,
-        category: 'Хиты продаж'       
+        category: 'Хиты продаж',
+        price: 23600       
     },
     {
         id:7,
         title: 'ЛОР оборудование',
         img: `${Photo7}`,
-        category: 'Акции'        
+        category: 'Акции'      ,
+        price: 402000,
+        oldprice:401000  
 
     },
     {
         id:8,
         title: 'Дерматологическое оборудование',
         img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR9JOXbKf08e-oxemFtmsvN-lUOdJ8yu3kAw&s`,  
-        category: 'Новинки'            
+        category: 'Новинки'    ,
+        price: 458000        
     },
     {
         id:9,
         title: 'Ультразвуковая диагностика',
         img: `https://media.istockphoto.com/id/1281389365/photo/blood-pressure-gauge.jpg?s=612x612&w=0&k=20&c=2vaSXGDF649H_UcFnNJ2YyJWmKY0p4FQDgk8PAKHZIA=` ,
-        category: 'Новинки'      
+        category: 'Новинки'    ,
+        price: 16000 
 
     },
     {
         id:10,
         title: 'Омрон БФ-212',
         img: `https://www.ciamedical.com/insights/content/uploads/2016/02/sss40.png` ,
-        category: 'Хиты продаж'       
+        category: 'Хиты продаж',
+        price: 23400       
     },
     {
         id:11,
         title: 'Диакомс РМ-2',
         img: `https://global-mt.ru/upload/iblock/a4b/84vuhast5808oq0an2ih27qevkqb8lwp.png`,
-        category: 'Новинки'              
+        category: 'Новинки'    ,
+        price: 320010          
     },
     {
         id:12,
         title: 'Ристер РИ-Чемпион',
         img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtoXFAE-2t6FAFbjxk6wPpWaETnRrClMGNpw&s`,
-        category: 'Акции'        
+        category: 'Акции'      ,
+        price: 785200,
+        oldprice: 788000  
 
     },
     {
         id:13,
         title: 'ЭлМед Троакар по Красножену',
         img: `https://www.ciamedical.com/insights/content/uploads/2016/02/sss72.png` ,
-        category: 'Хиты продаж'              
+        category: 'Хиты продаж',
+        price: 54860              
     },
     {
         id:14,
         title: 'Полимерные изделия окончатый 125 мм',
         img: `https://www.ciamedical.com/insights/content/uploads/2016/02/sss56.png`,
-        category: 'Новинки'              
+        category: 'Новинки'    ,
+        price:    89040       
     },
     {
         id:15,
         title: 'Биопсийные щипцы BIGopsy для ввода с дистального конца',
         img: `https://global-mt.ru/upload/iblock/c6e/0pordpcsv1m0eejjpk0uie7ftwusdl2k.jpg`,
-        category: 'Новинки'      
+        category: 'Новинки'    ,
+        price: 320450  
 
     },
     {
         id:16,
         title: 'Кровать медицинская МЕГИ МСК-122',
         img: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`,
-        category: 'Акции'        
+        category: 'Акции'      ,
+        price: 84860 ,
+        oldprice: 85000 
 
     },
     {
         id:17,
         title: 'Karl Storz Рабочий элемент по Kuntz',
         img: `https://global-mt.ru/upload/iblock/994/up35t6hkl0i48yv6vzobk5q776fmkfo3.png`,
-        category: 'Новинки'              
+        category: 'Новинки'    ,
+        price: 77500          
     },
     {
         id:18,
         title: 'Магнитно-резонансный томограф Canon Vantage Titan 3T',
         img: `https://global-mt.ru/upload/iblock/59a/ityne23377ryop131c7midefx80suksh.jpg`,
-        category: 'Хиты продаж'               
+        category: 'Хиты продаж',
+        price: 81400               
     },
     {
         id:19,
         title: 'ЛОР-комбайн Dantschke Intra 350 с закругленным профилем',
         img: `https://global-mt.ru/upload/iblock/f4f/x5ak53u35c2xyod0djun1lznt9ihcccs.jpg`,
-        category: 'Акции'        
+        category: 'Акции'      ,
+        price: 87400,
+        oldprice:88000  
         
     }, 
     {
         id:20,
         title: 'Монитор пациента Draeger Infinity M540',
         img: `https://global-mt.ru/upload/iblock/1b2/o4dt42ncygysdmmau7jr8rizq7uftkou.jpg` ,
-        category: 'Новинки'             
+        category: 'Новинки'    ,
+        price: 84750         
     },
     {
         id:21,
         title: 'Стоматологическая установка PD-852',
         img: `https://global-mt.ru/upload/iblock/d10/dz087gckve0isg016ueeqh728qarqs0s/delphis_f.webp`,
-        category: 'Хиты продаж'               
+        category: 'Хиты продаж',
+        price: 32500               
     },
     {
         id:22,
         title: 'Монитор пациента Welch Allyn ProPAQ LT',
         img: `https://global-mt.ru/upload/iblock/081/8qwdgwxyzfg37pzl307euom9zy22gelt.jpg`,
-        category: 'Акции'        
+        category: 'Акции'      ,
+        price: 42500,
+        oldprice: 43000  
 
     },
     {
         id:23,
         title: 'Клинический Е-индикатор',
         img: `https://global-mt.ru/upload/iblock/d0c/9qblkew2coh3bem6pn3iupvagg6gx8j3.jpg`,
-        category: 'Акции'        
+        category: 'Акции'      ,
+        price: 24500 ,
+        oldprice:25000 
          
     },
     {
         id:24,
         title: 'Хирургический микроскоп Leica Proveo 8',
         img: `https://global-mt.ru/upload/iblock/f78/7j6jox9nw5k2b89gjpa6asg4649l63se.png`,
-        category: 'Хиты продаж'               
+        category: 'Хиты продаж',
+        price: 45700               
     },
     {
         id:25,
         title: 'Кресло гинекологическое МЕГИ МСК-409',
         img: `https://global-mt.ru/upload/iblock/5b5/njqrvexqeeavhl6wrxtk279s0259v6cu.jpg`,
-        category: 'Акции'        
+        category: 'Акции'      ,
+        price: 35200 ,
+        oldprice: 36000 
 
     },
     {
         id:26,
         title: 'Медицинский монитор Barco Eonis 24" (MDRC-2324)',
         img: `https://global-mt.ru/upload/iblock/23a/tatflbuw7bewixs2qq5vje1tz519sdqj.jpg`,
-        category: 'Новинки'              
+        category: 'Новинки'    ,
+        price: 420240         
     },
     {
         id:27,
         title: 'Цифровой микроскоп DM-480A',
         img: `https://global-mt.ru/upload/iblock/ebf/aqgleom44nsherp43chdiy5ub93aybdd.jpg`,
-        category: 'Хиты продаж'               
+        category: 'Хиты продаж',
+        price: 45600               
     },
     {
         id:28,
         title: 'Хирургический микроскоп Haag-Streit Allegra 90',
         img: `https://global-mt.ru/upload/iblock/9f6/jcu7l3eet1tutps5l58q76zbvj7x2zj6.jpg`,
-        category: 'Хиты продаж'               
+        category: 'Хиты продаж',
+        price: 98000               
     },
     {
         id:29,
         title: 'НПФ Крыло ВК – 01 – «Крыло» тип 2',
         img: `https://global-mt.ru/upload/iblock/4e0/2rc7vtpsdfu8iynwpjcvgctfks97h830.jpg`,
-        category: 'Акции'        
+        category: 'Акции'      ,
+        price: 64000,
+        oldprice:64400
+
 
     },
     {
         id:30,
         title: 'Монитор пациента Philips IntelliVue MP90',
         img: `https://global-mt.ru/upload/iblock/0b9/bu398moo575z8w4bjc4ys2n3068n6g7q.png`,
-        category: 'Хиты продаж'               
+        category: 'Хиты продаж',
+        price: 31200               
     },
 ]
