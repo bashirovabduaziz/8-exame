d4cbda65b14bc057cc63c41453ee20b18f736bcb
bashirovabduaziz <bashirovabduaziz66@gmail.com>
2024-06-28 17:22:00 +0500

    forten day

--- src/pages/SinglePage/SinglePage.jsx
+++ src/pages/SinglePage/SinglePage.jsx
@@ -1,20 +1,78 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { productData } from '../../static/data';
 import { useParams } from 'react-router-dom';
+import { IoBarChartOutline, IoHeart, IoHeartOutline } from 'react-icons/io5';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleToWishes } from '../../context/wishlistSlice';
 
 const SinglePage = () => {
   const { id } = useParams();
   const productId = parseInt(id);
   const product = productData.find(item => item.id === productId);
+  const dispatch = useDispatch();
+  const wishes = useSelector(state => state.wishlist.value);
+  
+  const [mainImg, setMainImg] = useState(product.img);
+  const [thumbnails, setThumbnails] = useState([product.imgg, product.img2, product.img3]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    
-  }, [id]);
+    setMainImg(product.img);
+    setThumbnails([product.imgg, product.img2, product.img3]);
+  }, [id, product.img, product.imgg, product.img2, product.img3]);
+
+  const handleThumbnailClick = (index) => {
+    const newThumbnails = [...thumbnails];
+    const clickedImage = newThumbnails[index];
+    newThumbnails[index] = mainImg;
+    setMainImg(clickedImage);
+    setThumbnails(newThumbnails);
+  };
+
+  const getCategoryClass = (category) => {
+    switch (category) {
+      case 'Акции':
+        return 'border border-red-500 rounded-full p-[3px] text-red-500 ';
+      case 'Новинки':
+        return 'border border-green-500 rounded-full p-[3px] text-green-500 ';
+      case 'Хиты продаж':
+        return 'border border-blue-500 rounded-full p-[3px] text-blue-500';
+      default:
+        return '';
+    }
+  };
 
   return (
-    <div><img src={product.img} alt="" /></div>
+    <div className='mx-auto max-w-[1300px] px-5'>
+      <div className='flex flex-col gap-[15px] sm:gap-5 md:gap-[60px] md:flex-row md:justify-between py-[50px]  '>
+        <div className='lg:max-w-[700px] md:w-[600px] max-w-[360px] lg:h-[510px] md:h-[400px] h-[380px] bg-white border border-[#E5E2EE] rounded-xl'>
+          <div className='md:max-w-[550px] md:ml-[10px]  max-w-[360px]  flex md:justify-between justify-between  lg:ml-[30px] mt-[15px] items-center'>
+          <p className={` ${getCategoryClass(product.category)}  text-[12px] lg:text-[20px] md:text-[18px]` }>{product.category}</p>
+         <div className='flex sm:mr-[15px]'>
+         <button onClick={() => dispatch(toggleToWishes(product))} className='lg:mr-[20px] mr-[10px] '> {
+                        wishes.some(w => w.id === product.id) ?
+                            <IoHeart className='lg:w-[35px] md:h-[25px] lg:h-[35px] md:w-[25px] h-[18px] w-[18px] text-green-500' /> :
+                            <IoHeartOutline className='lg:w-[35px] md:h-[25px] lg:h-[35px] md:w-[25px] h-[18px] w-[18px]' />
+                    }</button>
+                     <button className=""><IoBarChartOutline className="lg:w-[35px] md:h-[25px] lg:h-[35px] md:w-[25px] h-[18px] w-[18px]" /></button>
+         </div>
+          </div>
+          <img src={mainImg} alt={product.title} className="lg:w-[500px] w-[280px] mt-[30px] ml-[20px] h-[250px] lg:h-[350px] lg:ml-[20px] lg:mt-[30px]" />
+          <div className='flex gap-[20px] lg:mt-[10px] lg:ml-[30px] ml-[20px] '>
+            {thumbnails.map((thumb, index) => (
+              <img 
+                key={index} 
+                src={thumb} 
+                className='w-[50px] h-[50px] cursor-pointer' 
+                alt="" 
+                onClick={() => handleThumbnailClick(index)} 
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default SinglePage
+export default SinglePage;
--- src/static/data.js
+++ src/static/data.js
@@ -1,9 +1,4 @@
-import Photo from '../../src/assets/Phtoto.png'
-import Photo2 from '../assets/Photo2.png'
-import Photo4 from '../assets/Photo4.png'
-import Photo5 from '../assets/Photo5.png'
-import Photo6 from '../assets/Photo6.png'
-import Photo7 from '../assets/Photo7.png'
+
 
 export const productData = [
     {
@@ -11,7 +6,10 @@ export const productData = [
         title: 'Анализатор мочи MIND UA-66',
         img: `https://global-mt.ru/upload/iblock/fac/2z16po15mw0bukk89j839sr5ra7m6kgx.jpg`,
         category: 'Новинки'    ,
-        price: 300000    
+        price: 300000 ,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`
     },
     {
         id:2,
@@ -19,21 +17,30 @@ export const productData = [
         img: `https://global-mt.ru/upload/iblock/85e/o3f2bd3fcsty3x0ykxn00ki55gjdkb3r/metrologya7.jpg`,
         category: 'Акции'      ,
         price: 24000,
-        oldprice: 28000  
+        oldprice: 28000  ,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`
     },
     {
         id:3,
         title: 'Эндоскопия',
         img: `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
         category: 'Новинки'    ,
-        price: 392300  
+        price: 392300  ,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`
     },
     {
         id:4,
         title: 'Оборудование для анастезиологии',
         img: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`,
         category: 'Новинки'    ,
-        price: 56000      
+        price: 56000   ,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`  
     },
     {
         id:5,
@@ -41,14 +48,20 @@ export const productData = [
         img: `https://global-mt.ru/upload/iblock/c5b/xtr9q6c9h5nsjdwx4tshrlt9v2eolok7.jpg`,
         category: 'Акции'      ,
         price: 220000 ,
-        oldprice:224000         
+        oldprice:224000,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`         
     },
     {
         id:6,
         title: 'Мебель для медецинских целей',
         img: `https://global-mt.ru/upload/iblock/494/53etmyp7htuaqvlggjf0xu2aogn5iwn3.jpg` ,
         category: 'Хиты продаж',
-        price: 23600       
+        price: 23600,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`       
     },
     {
         id:7,
@@ -56,7 +69,10 @@ export const productData = [
         img: `https://global-mt.ru/upload/iblock/b5e/grjkcyltbbdheo4e2kn9gila8zwh73v8.jpg`,
         category: 'Акции'      ,
         price: 402000,
-        oldprice:401000  
+        oldprice:401000 ,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg` 
 
     },
     {
@@ -64,14 +80,20 @@ export const productData = [
         title: 'Дерматологическое оборудование',
         img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR9JOXbKf08e-oxemFtmsvN-lUOdJ8yu3kAw&s`,  
         category: 'Новинки'    ,
-        price: 458000        
+        price: 458000 ,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`       
     },
     {
         id:9,
         title: 'Ультразвуковая диагностика',
         img: `https://media.istockphoto.com/id/1281389365/photo/blood-pressure-gauge.jpg?s=612x612&w=0&k=20&c=2vaSXGDF649H_UcFnNJ2YyJWmKY0p4FQDgk8PAKHZIA=` ,
         category: 'Новинки'    ,
-        price: 16000 
+        price: 16000,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg` 
 
     },
     {
@@ -79,14 +101,20 @@ export const productData = [
         title: 'Омрон БФ-212',
         img: `https://www.ciamedical.com/insights/content/uploads/2016/02/sss40.png` ,
         category: 'Хиты продаж',
-        price: 23400       
+        price: 23400,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`       
     },
     {
         id:11,
         title: 'Диакомс РМ-2',
         img: `https://global-mt.ru/upload/iblock/a4b/84vuhast5808oq0an2ih27qevkqb8lwp.png`,
         category: 'Новинки'    ,
-        price: 320010          
+        price: 320010,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`          
     },
     {
         id:12,
@@ -94,7 +122,10 @@ export const productData = [
         img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtoXFAE-2t6FAFbjxk6wPpWaETnRrClMGNpw&s`,
         category: 'Акции'      ,
         price: 785200,
-        oldprice: 788000  
+        oldprice: 788000,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`  
 
     },
     {
@@ -102,21 +133,30 @@ export const productData = [
         title: 'ЭлМед Троакар по Красножену',
         img: `https://www.ciamedical.com/insights/content/uploads/2016/02/sss72.png` ,
         category: 'Хиты продаж',
-        price: 54860              
+        price: 54860,
+        imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`              
     },
     {
         id:14,
         title: 'Полимерные изделия окончатый 125 мм',
         img: `https://www.ciamedical.com/insights/content/uploads/2016/02/sss56.png`,
         category: 'Новинки'    ,
-        price:    89040       
+        price:    89040 ,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`      
     },
     {
         id:15,
         title: 'Биопсийные щипцы BIGopsy для ввода с дистального конца',
         img: `https://global-mt.ru/upload/iblock/c6e/0pordpcsv1m0eejjpk0uie7ftwusdl2k.jpg`,
         category: 'Новинки'    ,
-        price: 320450  
+        price: 320450,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`  
 
     },
     {
@@ -124,7 +164,10 @@ export const productData = [
         title: 'Кровать медицинская МЕГИ МСК-122',
         img: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`,
         category: 'Акции'      ,
-        price: 84860 ,
+        price: 84860,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg` ,
         oldprice: 85000 
 
     },
@@ -133,14 +176,20 @@ export const productData = [
         title: 'Karl Storz Рабочий элемент по Kuntz',
         img: `https://global-mt.ru/upload/iblock/994/up35t6hkl0i48yv6vzobk5q776fmkfo3.png`,
         category: 'Новинки'    ,
-        price: 77500          
+        price: 77500,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`          
     },
     {
         id:18,
         title: 'Магнитно-резонансный томограф Canon Vantage Titan 3T',
         img: `https://global-mt.ru/upload/iblock/59a/ityne23377ryop131c7midefx80suksh.jpg`,
         category: 'Хиты продаж',
-        price: 81400               
+        price: 81400,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`               
     },
     {
         id:19,
@@ -148,6 +197,9 @@ export const productData = [
         img: `https://global-mt.ru/upload/iblock/f4f/x5ak53u35c2xyod0djun1lznt9ihcccs.jpg`,
         category: 'Акции'      ,
         price: 87400,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`,
         oldprice:88000  
         
     }, 
@@ -156,14 +208,20 @@ export const productData = [
         title: 'Монитор пациента Draeger Infinity M540',
         img: `https://global-mt.ru/upload/iblock/1b2/o4dt42ncygysdmmau7jr8rizq7uftkou.jpg` ,
         category: 'Новинки'    ,
-        price: 84750         
+        price: 84750,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`         
     },
     {
         id:21,
         title: 'Стоматологическая установка PD-852',
         img: `https://global-mt.ru/upload/iblock/d10/dz087gckve0isg016ueeqh728qarqs0s/delphis_f.webp`,
         category: 'Хиты продаж',
-        price: 32500               
+        price: 32500,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`               
     },
     {
         id:22,
@@ -171,6 +229,9 @@ export const productData = [
         img: `https://global-mt.ru/upload/iblock/081/8qwdgwxyzfg37pzl307euom9zy22gelt.jpg`,
         category: 'Акции'      ,
         price: 42500,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`,
         oldprice: 43000  
 
     },
@@ -179,7 +240,10 @@ export const productData = [
         title: 'Клинический Е-индикатор',
         img: `https://global-mt.ru/upload/iblock/d0c/9qblkew2coh3bem6pn3iupvagg6gx8j3.jpg`,
         category: 'Акции'      ,
-        price: 24500 ,
+        price: 24500,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg` ,
         oldprice:25000 
          
     },
@@ -188,14 +252,20 @@ export const productData = [
         title: 'Хирургический микроскоп Leica Proveo 8',
         img: `https://global-mt.ru/upload/iblock/f78/7j6jox9nw5k2b89gjpa6asg4649l63se.png`,
         category: 'Хиты продаж',
-        price: 45700               
+        price: 45700,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`               
     },
     {
         id:25,
         title: 'Кресло гинекологическое МЕГИ МСК-409',
         img: `https://global-mt.ru/upload/iblock/5b5/njqrvexqeeavhl6wrxtk279s0259v6cu.jpg`,
         category: 'Акции'      ,
-        price: 35200 ,
+        price: 35200,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg` ,
         oldprice: 36000 
 
     },
@@ -204,21 +274,30 @@ export const productData = [
         title: 'Медицинский монитор Barco Eonis 24" (MDRC-2324)',
         img: `https://global-mt.ru/upload/iblock/23a/tatflbuw7bewixs2qq5vje1tz519sdqj.jpg`,
         category: 'Новинки'    ,
-        price: 420240         
+        price: 420240 ,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`        
     },
     {
         id:27,
         title: 'Цифровой микроскоп DM-480A',
         img: `https://global-mt.ru/upload/iblock/ebf/aqgleom44nsherp43chdiy5ub93aybdd.jpg`,
         category: 'Хиты продаж',
-        price: 45600               
+        price: 45600,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`               
     },
     {
         id:28,
         title: 'Хирургический микроскоп Haag-Streit Allegra 90',
         img: `https://global-mt.ru/upload/iblock/9f6/jcu7l3eet1tutps5l58q76zbvj7x2zj6.jpg`,
         category: 'Хиты продаж',
-        price: 98000               
+        price: 98000,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`               
     },
     {
         id:29,
@@ -226,6 +305,9 @@ export const productData = [
         img: `https://global-mt.ru/upload/iblock/4e0/2rc7vtpsdfu8iynwpjcvgctfks97h830.jpg`,
         category: 'Акции'      ,
         price: 64000,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`,
         oldprice:64400
 
 
@@ -235,6 +317,9 @@ export const productData = [
         title: 'Монитор пациента Philips IntelliVue MP90',
         img: `https://global-mt.ru/upload/iblock/0b9/bu398moo575z8w4bjc4ys2n3068n6g7q.png`,
         category: 'Хиты продаж',
-        price: 31200               
+        price: 31200,
+          imgg:  'https://global-mt.ru/upload/iblock/9a2/mfkne7gdl927ydtlb17hu6upe433xqm6.jpg',
+        img2:    `https://global-mt.ru/upload/iblock/b39/0o4wfxmcinlhszb86sgnuymbxesi3w7g.jpg`,
+        img3: `https://global-mt.ru/upload/iblock/343/thhoixipw9z1qmr1d532z5xs0r698m68.jpg`               
     },
 ]
