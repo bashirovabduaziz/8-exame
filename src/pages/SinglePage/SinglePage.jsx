dd04814a8e6cf3661f7d5eed26e2b168ce4e11ad
bashirovabduaziz <bashirovabduaziz66@gmail.com>
2024-06-28 20:04:53 +0500

    fortyn day yarimi

--- src/pages/SinglePage/SinglePage.jsx
+++ src/pages/SinglePage/SinglePage.jsx
@@ -44,9 +44,9 @@ const SinglePage = () => {
 
   return (
     <div className='mx-auto max-w-[1300px] px-5'>
-      <div className='flex flex-col gap-[15px] sm:gap-5 md:gap-[60px] md:flex-row md:justify-between py-[50px]  '>
-        <div className='lg:max-w-[700px] md:w-[600px] max-w-[360px] lg:h-[510px] md:h-[400px] h-[380px] bg-white border border-[#E5E2EE] rounded-xl'>
-          <div className='md:max-w-[550px] md:ml-[10px]  max-w-[360px]  flex md:justify-between justify-between  lg:ml-[30px] mt-[15px] items-center'>
+      <div className='lg:flex flex-col gap-[15px] sm:gap-5 md:gap-[60px] md:flex-col  md:justify-between py-[50px]  '>
+        <div className='lg:max-w-[800px] md:flex-col md:max-w-[1020px] max-w-[700px]  lg:h-[510px] md:h-[560px] h-[380px] bg-white border border-[#E5E2EE] rounded-xl overflow-hidden sm:flex-col sm:items-center  '>
+          <div className='md:max-w-[700px] lg:max-w-[700px]  max-w-[300px] mx-auto  flex  justify-between  mt-[15px] items-center'>
           <p className={` ${getCategoryClass(product.category)}  text-[12px] lg:text-[20px] md:text-[18px]` }>{product.category}</p>
          <div className='flex sm:mr-[15px]'>
          <button onClick={() => dispatch(toggleToWishes(product))} className='lg:mr-[20px] mr-[10px] '> {
@@ -57,18 +57,21 @@ const SinglePage = () => {
                      <button className=""><IoBarChartOutline className="lg:w-[35px] md:h-[25px] lg:h-[35px] md:w-[25px] h-[18px] w-[18px]" /></button>
          </div>
           </div>
-          <img src={mainImg} alt={product.title} className="lg:w-[500px] w-[280px] mt-[30px] ml-[20px] h-[250px] lg:h-[350px] lg:ml-[20px] lg:mt-[30px]" />
-          <div className='flex gap-[20px] lg:mt-[10px] lg:ml-[30px] ml-[20px] '>
+          <img src={mainImg} alt={product.title} className="lg:w-[600px] w-[300px] md:w-[600px] md:h-[400px] mt-[30px] h-[250px] lg:h-[350px]  lg:mt-[30px] mx-auto block" />
+          <div className='max-w-[200px]  mx-auto  flex  justify-between lg:mt-[10px] lg:ml-[60px]   '>
             {thumbnails.map((thumb, index) => (
               <img 
                 key={index} 
                 src={thumb} 
-                className='w-[50px] h-[50px] cursor-pointer' 
+                className='w-[50px] h-[50px] cursor-pointer border border-[#E5E2EE] rounded-[5px]' 
                 alt="" 
                 onClick={() => handleThumbnailClick(index)} 
               />
             ))}
           </div>
+        </div>
+        <div>
+          
         </div>
       </div>
     </div>
